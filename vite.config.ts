0c794d4d2f183d80a4b6ade8d670494561cbf3df
ctardy <ctardy@students.cs.ubc.ca>
2025-12-23 18:02:21 -0500

    config modifications

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace 'hangman' with your exact repository name if different
 export default defineConfig({
   plugins: [react()],
+  base: '/hangman/', 
 })
